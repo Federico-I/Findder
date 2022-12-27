ae37153d2e295ca6a3dbf8e8869c9abd95582883
Threxos <threxos.48@gmail.com>
2022-12-27 14:47:27 +0100

    setiing-up-Reducers-useReducer

--- src/Components/Context/GitHub/GitHubContext.js
+++ src/Components/Context/GitHub/GitHubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
+import githubReducers from "./GitHubReducers";
 
 const GitHubContext = createContext();
 
@@ -6,8 +7,17 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GitHubProvider = ({ children }) => {
+  /*
   const [displayUsers, setDisplayUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+    */
+
+  const initialState = {
+    displayUsers: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducers, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,15 +28,20 @@ export const GitHubProvider = ({ children }) => {
 
     const data = await response.json();
 
-    setDisplayUsers(data);
-    setLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+
+    /*setDisplayUsers(data);
+    setLoading(false);*/
   };
 
   return (
     <GitHubContext.Provider
       value={{
-        displayUsers,
-        loading,
+        displayUsers: state.displayUsers,
+        loading: state.loading,
         fetchUsers,
       }}
     ></GitHubContext.Provider>
